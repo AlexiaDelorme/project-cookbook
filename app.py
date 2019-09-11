7f5379eefb88e508117a615cdf452ca5de04b29b
Alexia Delorme <alexia.delorme@gmail.com>
2019-09-11 14:33:43 +0000

    Added carousel for favorite recipes in recipes template

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from helpers import *
+
 
 
 app = Flask(__name__)
@@ -11,27 +13,37 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 
+
 @app.route('/')
 def home():
     return render_template("home.html", Page_title = "The Pâtisserie Journal", Welcome_image = "../static/img/home-bg.jpg")
-    
+
+  
 @app.route('/recipes')
 def recipes():
-    return render_template("recipes.html", Page_title = "Discover recipes by...", Welcome_image = "../static/img/recipes-bg.jpg", categories = ["Categories", "Diet", "Geography", "Occasions"])
+    return render_template("recipes.html", 
+    Page_title = "Discover recipes by...", 
+    Welcome_image = "../static/img/recipes-bg.jpg", 
+    categories = mongo.db.recipes_categories.find(), 
+    carousel = image_folder('carousel'))
+
 
 @app.route('/recipes/brownie')
 def brownie():
     return render_template("brownie.html", recipes=mongo.db.recipes_information.find())
 
+
 @app.route('/account')
 def account():
     return render_template('account.html')
+ 
     
 @app.route('/insert_user_account', methods=['POST'])
 def insert_user_account():
     user_accounts = mongo.db.user_accounts
     user_accounts.insert_one(request.form.to_dict())
     return "Thanks for signing up!"
+
 
 
 if __name__ == '__main__':
