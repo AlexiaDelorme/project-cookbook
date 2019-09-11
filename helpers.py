e28867d5bdc36eb321214d397ada60236062bab0
Alexia Delorme <alexia.delorme@gmail.com>
2019-09-11 14:31:09 +0000

    Created helper functions file

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import os
+
+def image_folder(folder):
+    list_of_image = []
+    for filename in os.listdir('static/img/'+folder):
+        if filename.endswith(".jpg"):
+            list_of_image.append(os.path.join('static/img/'+folder+'/'+ filename))
+        else:
+            continue
+    return list_of_image
+
+# print(image_folder('carousel'))
+
