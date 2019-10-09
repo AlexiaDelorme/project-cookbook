fb9a819cadd6de664646f37d96345db1bb92ac19
Alexia Delorme <alexia.delorme@gmail.com>
2019-10-09 10:17:15 +0000

    Created new form to update password for user

--- forms.py
+++ forms.py
@@ -22,3 +22,11 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
+
+
+class PasswordForm(FlaskForm):
+    current_password = PasswordField("Current Password", validators=[DataRequired()])
+    new_password = PasswordField("New Password", validators=[DataRequired()])
+    confirm_new_password = PasswordField("Confirm New Password",
+                                     validators=[DataRequired(), EqualTo("new_password")])
+    submit = SubmitField("Update Password")                                 
