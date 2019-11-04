40ada582bb458beae75ead12c24ec24693627b7b
Alexia Delorme <alexia.delorme@gmail.com>
2019-11-04 15:22:21 +0000

    Added following criteria for query fiters when explore form is submitted: serving, meal, diet, allergen, tool, occasion, geography

--- app.py
+++ app.py
@@ -68,7 +68,15 @@ def explore_results():
     Display all the recipes matching the criteria selected in the form from the explore page.
     """
     recipes = mongo.db.recipes_information.find({
-                                                "difficulty": request.form.get("difficulty")
+                                                "difficulty": request.form.get("difficulty"),
+                                                "serving": { "$lte": request.form.get("serving") },
+                                                # "prep_time": { TBD },
+                                                "meal": { "$in": request.form.getlist("meal") },
+                                                "diet": { "$in": request.form.getlist("diet") },
+                                                "allergen": { "$nin": request.form.getlist("allergen") },
+                                                "tool": { "$in": request.form.getlist("tool") },
+                                                "occasion": { "$in": request.form.getlist("occasion") },
+                                                "geography": { "$in": request.form.getlist("geography") }
                                                 })
     recipes_number=recipes.count()
     return render_template("results.html",
