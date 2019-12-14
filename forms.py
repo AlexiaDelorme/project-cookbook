23bea2338a6595013d6139907d610b9e386e4bc9
Alexia Delorme <alexia.delorme@gmail.com>
2019-12-14 13:49:12 +0000

    Added mandatory fields to edit password form

--- forms.py
+++ forms.py
@@ -25,8 +25,8 @@ class LoginForm(FlaskForm):
 
 
 class PasswordForm(FlaskForm):
-    current_password = PasswordField("Current Password", validators=[DataRequired()])
-    new_password = PasswordField("New Password", validators=[DataRequired()])
-    confirm_new_password = PasswordField("Confirm New Password",
+    current_password = PasswordField('Current Password<span class="red-text">*</span>', validators=[DataRequired()])
+    new_password = PasswordField('New Password<span class="red-text">*</span>', validators=[DataRequired()])
+    confirm_new_password = PasswordField('Confirm New Password<span class="red-text">*</span>',
                                      validators=[DataRequired(), EqualTo("new_password")])
     submit = SubmitField("Update Password")     
