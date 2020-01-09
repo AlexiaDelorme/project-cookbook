95ed7b620253c8c858338d1267b707f44b80e293
Alexia Delorme <alexia.delorme@gmail.com>
2020-01-09 17:43:12 +0000

    Removed pagination function from helpers

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 import os
 from flask import request
-from flask_paginate import Pagination, get_page_parameter
 
 # print(image_folder('carousel'))
 def image_folder(folder):
@@ -11,17 +10,4 @@ def image_folder(folder):
         else:
             continue
     return list_of_image
-
-# Set up pagination for recipes results - Code credit: https://pythonhosted.org/Flask-paginate/
-def pagination_function(recipes):
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-    recipes_number = recipes.count()
-    pagination = Pagination(page=page, total=recipes_number, search=search, record_name='recipes')
-    return pagination
-
-
 
