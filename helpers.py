1a7d6719e63120d17fb539a0fe25c6af8829beaf
Alexia Delorme <alexia.delorme@gmail.com>
2020-01-10 09:45:34 +0000

    After fixing code for pagination thanks to tutor team, refactored into a function so it could be invoked several times from my app.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import os
 from flask import request
+from flask_paginate import Pagination, get_page_args
 
 # print(image_folder('carousel'))
 def image_folder(folder):
@@ -10,4 +11,20 @@ def image_folder(folder):
         else:
             continue
     return list_of_image
+
+# Set up pagination for recipes results - Code credit: https://pythonhosted.org/Flask-paginate/
+def pagination_function(query_recipes):
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    number_to_skip = (page-1)*per_page
+    recipes = query_recipes.skip(number_to_skip).limit(per_page)
+    recipes_number = query_recipes.count()
+    pagination = Pagination(page=page, total=recipes_number, search=search, record_name='recipes')
+    return {"pagination": pagination, 
+            "recipes": recipes, 
+            "recipes_number": recipes_number}
 
