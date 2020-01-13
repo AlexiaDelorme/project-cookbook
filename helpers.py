2f5d8643da895949f6f5751653e475395779efab
Alexia Delorme <alexia.delorme@gmail.com>
2020-01-13 13:15:57 +0000

    Removed unused function and made the file PEP8 compliant

--- helpers.py
+++ helpers.py
@@ -2,17 +2,9 @@ import os
 from flask import request
 from flask_paginate import Pagination, get_page_args
 
-# print(image_folder('carousel'))
-def image_folder(folder):
-    list_of_image = []
-    for filename in os.listdir('static/img/'+folder):
-        if filename.endswith(".jpg"):
-            list_of_image.append(os.path.join('static/img/'+folder+'/'+ filename))
-        else:
-            continue
-    return list_of_image
 
-# Set up pagination for recipes results - Code credit: https://pythonhosted.org/Flask-paginate/
+# Set up pagination for recipes results
+# Code credit: https://pythonhosted.org/Flask-paginate/
 def pagination_function(query_recipes):
     search = False
     q = request.args.get('q')
@@ -23,8 +15,10 @@ def pagination_function(query_recipes):
     number_to_skip = (page-1)*per_page
     recipes = query_recipes.skip(number_to_skip).limit(per_page)
     recipes_number = query_recipes.count()
-    pagination = Pagination(page=page, total=recipes_number, search=search, record_name='recipes')
-    return {"pagination": pagination, 
-            "recipes": recipes, 
+    pagination = Pagination(page=page,
+                            total=recipes_number,
+                            search=search,
+                            record_name='recipes')
+    return {"pagination": pagination,
+            "recipes": recipes,
             "recipes_number": recipes_number}
-
