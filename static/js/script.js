6fda125247879bc8a3154ef06c1ce1d874e0dba9
Alexia Delorme <alexia.delorme@gmail.com>
2020-01-27 18:47:51 +0000

    Magnified js scrip comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
 
-    // jQUERY SCRIPT
+    /*
+    --------------
+        jQUERY
+    --------------
+    */
 
-    // jQuery for materialize components
+    /*
+        Initialize Materialize Components 
+    */
     $(".collapsible").collapsible();
     $(".button-collapse").sideNav();
     $(".parallax").parallax();
@@ -10,41 +16,52 @@ $(document).ready(function () {
     $("select").material_select();
     $(".modal").modal();
 
-    // Add center-align class for pagination after DOM is loaded
+    /*
+        Center pagination after page loaded 
+    */   
     $(".pagination-page-info").addClass("center-align");
     $(".pagination").addClass("center-align");
 
-    // Display difficulty selected by user
+    /*
+        Display feedback to user for difficulty select field
+    */  
     $("select#difficulty").change(function () {
         var selectedOption = $(this).children("option:selected").val();
         if (selectedOption == "") {
             $(this).parent(".select-wrapper").children("input").css({ "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" });
         } else {
             $(this).parent(".select-wrapper").children("input").css({ "border-bottom": "1px solid #4CAF50", "box-shadow": "0 1px 0 0 #4CAF50" });
-        };
+        }
     });
 
-    // Remove 'required' attribute for minutes if hours
+    /*
+        Remove 'required' attribute for minutes if 'hours' field is not empty
+    */
     $("#hours").change(function () {
         var inputtedHour = $(this).val();
-        console.log(inputtedHour);
         if (inputtedHour == "" || inputtedHour == 0) {
             $("#minutes").prop("required", true);
         } else {
             $("#minutes").prop("required", false);
-        };
+        }
     });
 
-    // Hide all elements with this class on page load
+    /*
+        Hide all elements with this class after page loaded
+    */
     $(".add-item-error").hide();
-
-    // Hide feedback when user is typing in the field
+    
+    /*
+        Hide all manual feedbacks when user is typing in the field
+    */
     $(document).on('keyup', '.manual-feedback', function () {
         $(this).parent("div").parent("div").siblings(".add-item-error").hide();
         $(this).siblings(".alert-div").removeClass("data-error-manual").removeClass("data-success-manual").text("");
     });
 
-    // Function to provide manual feedback when field is changed
+    /*
+        Function to provide manual feedback when ingredient/instruction field is being changed
+    */
     function feedbackChangeFunction() {
         $(".manual-feedback").change(function () {
             var field = $(this).val();
@@ -52,11 +69,13 @@ $(document).ready(function () {
                 $(this).siblings(".alert-div").removeClass("data-success-manual").addClass("data-error-manual").text("*required");
             } else {
                 $(this).siblings(".alert-div").removeClass("data-error-manual").addClass("data-success-manual").text("validated");
-            };
+            }
         });
-    };
+    }
 
-    // Function to provide manual feedback when user focuses out of field
+    /*
+        Function to provide manual feedback when user is focusing out of ingredient/instruction field
+    */
     function feedbackFocusFunction() {
         $(".manual-feedback").focusout(function () {
             var field = $(this).val();
@@ -64,14 +83,16 @@ $(document).ready(function () {
                 $(this).siblings(".alert-div").removeClass("data-success-manual").addClass("data-error-manual").text("*required");
             } else {
                 $(this).siblings(".alert-div").removeClass("data-error-manual").addClass("data-success-manual").text("validated");
-            };
+            }
         });
-    };
+    }
 
     feedbackFocusFunction();
     feedbackChangeFunction();
 
-    // Function to check if last ingredient field is empty before adding a new one
+    /*
+        Function to check if last ingredient field is empty before adding a new one
+    */
     $("#add-ingredients").click(function () {
         var lastIngredientField = $("#ingredientsform").children("div:last-child").children("textarea").val();
         if (lastIngredientField == "") {
@@ -79,10 +100,12 @@ $(document).ready(function () {
         } else {
             $("#data-error-ingredients").hide();
             addIngredientsFunction();
-        };
+        }
     });
 
-    // Function to add a new ingredient to the list
+    /*
+        Function to add a new ingredient field in the list by creating a new fieldWrapper
+    */
     function addIngredientsFunction() {
         var fieldWrapper = $("<div class=\"fieldwrapper\"/>");
         var fName = $("<textarea id=\"ingredients\" name=\"ingredients\" minlength=\"1\" maxlength=\"100\" class=\"validate materialize-textarea manual-feedback col s11\" placeholder=\"Example: Milk, Eggs, Chocolate, Flour\" required></textarea>");
@@ -95,12 +118,14 @@ $(document).ready(function () {
         fieldWrapper.append(removeButton);
         fieldWrapper.append(alertDiv);
         $("#ingredientsform").append(fieldWrapper);
-        // Add event listeners when new item is created
+        // Add event listeners when a new item is created
         feedbackFocusFunction();
         feedbackChangeFunction();
-    };
+    }
 
-    // Function to check if last instruction field is empty before adding a new one
+    /*
+        Function to check if last ingredient field is empty before adding a new one
+    */
     $("#add-instructions").click(function () {
         var lastInstructionField = $("#instructionsform").children("div:last-child").children("textarea").val();
         if (lastInstructionField == "") {
@@ -108,10 +133,12 @@ $(document).ready(function () {
         } else {
             $("#data-error-instructions").hide();
             addInstructionsFunction();
-        };
+        }
     });
 
-    // Function to add new instructions
+    /*
+        Function to add a new instruction field in the list by creating a new fieldWrapper
+    */
     function addInstructionsFunction() {
         var fieldWrapper = $("<div class=\"fieldwrapper\"/>");
         var fName = $("<textarea id=\"instructions\" name=\"instructions\" minlength=\"1\" maxlength=\"500\" class=\"validate materialize-textarea manual-feedback col s11\" placeholder=\"Example: In a large bowl, mix all the wet ingregients\" required></textarea>");
@@ -124,26 +151,37 @@ $(document).ready(function () {
         fieldWrapper.append(removeButton);
         fieldWrapper.append(alertDiv);
         $("#instructionsform").append(fieldWrapper);
-        // Add event listener when new item is created
+        // Add event listeners when a new item is created
         feedbackFocusFunction();
         feedbackChangeFunction();
-    };
+    }
 
-    // Remove existing ingredients/instructions when user edits a recipe
+    /*
+        Remove existing ingredient/instruction field when user edits a recipe
+    */
     $(".remove-btn").click(function () {
         $(this).parent().remove();
     });
 
 });
 
-// CORE JS SCRIPT
+/*
+---------------
+    CORE JS
+---------------
+*/
 
-// Function to print recipes
+/*
+    Function to print recipe description
+*/
 function myPrintFunction() {
     window.print();
 }
 
-// Use Sweet Alert to create a two-tier confirmation before deleting the recipe
+/*
+    Sweet Alert Library
+    Create a two-tier confirmation before deleting a recipe
+*/
 function deleteRecipeFunction(recipeId) {
     swal({
         title: "Are you sure?",
@@ -163,6 +201,7 @@ function deleteRecipeFunction(recipeId) {
                     swal("Request to delete recipe sent!", {
                         icon: "success"
                     })
+                        // Create event trigger to submit the form only if user confirmed it
                         .then((value) => { document.getElementById(recipeId + "Form").submit(); });
                     break;
                 default:
@@ -171,7 +210,10 @@ function deleteRecipeFunction(recipeId) {
         });
 }
 
-// Use Sweet Alert to create two-tier confirmation before deleting an account
+/*
+    Sweet Alert Library
+    Create a two-tier confirmation before deleting an account
+*/
 function deleteAccountFunction() {
     swal({
         title: "Are you sure?",
@@ -191,6 +233,7 @@ function deleteAccountFunction() {
                     swal("Request to delete your account sent!", {
                         icon: "success"
                     })
+                        // Create event trigger to submit the form only if user confirmed it
                         .then((value) => { document.getElementById("deleteAccountForm").submit(); });
                     break;
                 default:
